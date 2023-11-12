a800b3a9c2569982bdb342d5cbc5d546cf8758f0
Giovane Souza <developergiovanesouza@gmail.com>
2023-11-12 13:53:29 -0300

    fix: remoção código duplicado validação

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -47,14 +47,7 @@ const findAllUsersController = async (req, res) => {
 
 const updateUserController = async (req, res) => {
     try{
-        const body = req.body;
-        
-        if(!body.nome){
-            return res.status(400).send({ message: `O campo 'nome' precisa ser preenchido!`});
-        }
-
         return res.send(await userService.updateUserService(req.params.id, body));
-
     }catch (err){
         console.log(`erro: ${err.message}`);
         return res.status(500).send({ message: `Erro inesperado tente novamente!`});  
