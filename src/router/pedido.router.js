d0298bdfc09c2b37ef28f282c61876798ee7cef6
Giovane Souza <developergiovanesouza@gmail.com>
2023-11-15 16:32:32 -0300

    feat: add validaBodyId

--- src/router/pedido.router.js
+++ src/router/pedido.router.js
@@ -10,7 +10,7 @@ router.post("/create", authMiddleware, validaPedido, pedidoController.createOrde
 router.get("/findById/:id", authMiddleware, validaIdParams, pedidoController.findOrderByIdController);
 
 router.get("/findAll", authMiddleware, pedidoController.findAllOrdersController);
-router.get("/findAllOrdersByUserId", authMiddleware, pedidoController.findAllOrdersByUserIdController);
+router.get("/findAllOrdersByUserId", authMiddleware, validaBodyId, pedidoController.findAllOrdersByUserIdController);
 router.get("/getOrderInfoById", authMiddleware, validaBodyId, pedidoController.getOrderInfoByIdController);
 
 router.delete("/delete/:id", authMiddleware, validaIdParams, pedidoController.deleteOrderController);
