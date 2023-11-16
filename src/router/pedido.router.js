2ce197b1e0e6daccee04257e54b9bca13c71b1de
Giovane Souza <developergiovanesouza@gmail.com>
2023-11-15 21:36:45 -0300

    feat: verificação admin - só admin pode atualizar

--- src/router/pedido.router.js
+++ src/router/pedido.router.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 
 const pedidoController = require("../controller/pedido.controller");
 const authMiddleware = require("../middleware/auth.middleware");
-const { validaPedido,  validaIdParams, validaBodyId, validaProdutosCarrinhoPedido } = require("../middleware/validacao.middleware");
+const validaAdmin = require("../middleware/valida.admin.middleware"); // Verifica se é Admin
+const { validaPedido,  validaIdParams, validaBodyId } = require("../middleware/validacao.middleware");
 
 
 router.post("/create", authMiddleware, validaPedido, pedidoController.createOrderController);
@@ -15,7 +16,7 @@ router.get("/getOrderInfoById", authMiddleware, validaBodyId, pedidoController.g
 
 router.delete("/delete/:id", authMiddleware, validaIdParams, pedidoController.deleteOrderController);
 
-router.patch("/updateStatus/:id", authMiddleware, validaIdParams, pedidoController.updateOrderStatusController);
+router.patch("/updateStatus/:id", authMiddleware, validaAdmin, validaIdParams, pedidoController.updateOrderStatusController);
 
 
 module.exports = router;
